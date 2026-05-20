a160326d4e63a58b9a244b7fe7912f07c68f43df
pcvargas <pcvargas@gmail.com>
2026-05-19 19:14:04 -0500

    Update extension.js

--- extension.js
+++ extension.js
@@ -1,7 +1,7 @@
 var extension = {
   name: "ADS1115",
-  description: "ADC I2C 16-bit, 4 canales — KidBright32",
-  author: "Academia Gadget",
+  description: "ADC I2C 16-bit, 4 canales para KidBright32",
+  author: "pcvargas",
   version: "1.0.0",
   color: "#006a99",
   category: "Sensors",
@@ -13,12 +13,12 @@ var extension = {
   modules: [
     "modules/ADS1115.py"
   ],
-  toolbox: '<category name="ADS1115" colour="230">' +
-    '<block type="ADS1115_begin"><field name="ADDR">0x48</field></block>' +
-    '<block type="ADS1115_set_gain"><field name="GAIN">2</field></block>' +
-    '<block type="ADS1115_read_raw"><field name="CHANNEL">0</field></block>' +
-    '<block type="ADS1115_read_mv"><field name="CHANNEL">0</field></block>' +
-    '<block type="ADS1115_read_v"><field name="CHANNEL">0</field></block>' +
-    '<block type="ADS1115_read_percent"><field name="CHANNEL">0</field></block>' +
-    '</category>'
+  toolbox: '<category name="ADS1115" colour="230">'
+    + '<block type="ADS1115_begin"><field name="ADDR">0x48</field></block>'
+    + '<block type="ADS1115_set_gain"><field name="GAIN">2</field></block>'
+    + '<block type="ADS1115_read_raw"><field name="CHANNEL">0</field></block>'
+    + '<block type="ADS1115_read_mv"><field name="CHANNEL">0</field></block>'
+    + '<block type="ADS1115_read_v"><field name="CHANNEL">0</field></block>'
+    + '<block type="ADS1115_read_percent"><field name="CHANNEL">0</field></block>'
+    + '</category>'
 };
