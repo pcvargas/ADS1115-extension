6b83dcc69b6d3267ba33dcc8245341b337f244d6
pcvargas <pcvargas@gmail.com>
2026-05-19 19:23:21 -0500

    Update extension.js

--- extension.js
+++ extension.js
@@ -1,24 +1,16 @@
-var extension = {
-  name: "ADS1115",
-  description: "ADC I2C 16-bit, 4 canales para KidBright32",
-  author: "pcvargas",
-  version: "1.0.0",
-  color: "#006a99",
-  category: "Sensors",
-  icon: "static/icon.png",
-  blocks: [
-    "blocks/blocks_ADS1115.js",
-    "blocks/generators_ADS1115.js"
-  ],
-  modules: [
-    "modules/ADS1115.py"
-  ],
-  toolbox: '<category name="ADS1115" colour="230">'
-    + '<block type="ADS1115_begin"><field name="ADDR">0x48</field></block>'
-    + '<block type="ADS1115_set_gain"><field name="GAIN">2</field></block>'
-    + '<block type="ADS1115_read_raw"><field name="CHANNEL">0</field></block>'
-    + '<block type="ADS1115_read_mv"><field name="CHANNEL">0</field></block>'
-    + '<block type="ADS1115_read_v"><field name="CHANNEL">0</field></block>'
-    + '<block type="ADS1115_read_percent"><field name="CHANNEL">0</field></block>'
-    + '</category>'
+module.exports = {
+    id: "ads1115-extension", // ¡Obligatorio! Define el nombre de la carpeta de instalación
+    name: "ADS1115 Pro",
+    description: "Módulo ADC de alta precisión I2C para sensores analógicos",
+    category: "Sensores",
+    author: "Pablo Vargas",
+    version: "1.0.0",
+    color: "#8E44AD",
+    icon: "icon.png",
+    blocks: [
+        "ads1115_init",
+        {
+            xml: `<block type="ads1115_read"><field name="CHANNEL">0</field></block>`
+        }
+    ]
 };
