2c03c0335d9b28ddfc0cdac43e39988cffd030ee
pcvargas <pcvargas@gmail.com>
2026-05-20 09:56:56 -0500

    Update generators_ADS1115.js

--- blocks/generators_ADS1115.js
+++ blocks/generators_ADS1115.js
@@ -1,24 +1,6 @@
-Blockly.Python["ADS1115_begin"] = function(block) {
-  Blockly.Python.definitions_["import_ADS1115"] = "import ADS1115";
-  return "ADS1115.begin(" + block.getFieldValue("ADDR") + ")\n";
-};
-Blockly.Python["ADS1115_set_gain"] = function(block) {
-  Blockly.Python.definitions_["import_ADS1115"] = "import ADS1115";
-  return "ADS1115.set_gain(" + block.getFieldValue("GAIN") + ")\n";
-};
-Blockly.Python["ADS1115_read_raw"] = function(block) {
-  Blockly.Python.definitions_["import_ADS1115"] = "import ADS1115";
-  return ["ADS1115.read_raw(" + block.getFieldValue("CHANNEL") + ")", Blockly.Python.ORDER_FUNCTION_CALL];
-};
-Blockly.Python["ADS1115_read_mv"] = function(block) {
-  Blockly.Python.definitions_["import_ADS1115"] = "import ADS1115";
-  return ["ADS1115.read_voltage(" + block.getFieldValue("CHANNEL") + ")", Blockly.Python.ORDER_FUNCTION_CALL];
-};
-Blockly.Python["ADS1115_read_v"] = function(block) {
-  Blockly.Python.definitions_["import_ADS1115"] = "import ADS1115";
-  return ["ADS1115.read_voltage_v(" + block.getFieldValue("CHANNEL") + ")", Blockly.Python.ORDER_FUNCTION_CALL];
-};
-Blockly.Python["ADS1115_read_percent"] = function(block) {
-  Blockly.Python.definitions_["import_ADS1115"] = "import ADS1115";
-  return ["ADS1115.read_percent(" + block.getFieldValue("CHANNEL") + ")", Blockly.Python.ORDER_FUNCTION_CALL];
-};
+Blockly.Python["ADS1115_begin"]=function(b){Blockly.Python.definitions_["import_ADS1115"]="import ADS1115";return"ADS1115.begin("+b.getFieldValue("ADDR")+")\n";};
+Blockly.Python["ADS1115_set_gain"]=function(b){Blockly.Python.definitions_["import_ADS1115"]="import ADS1115";return"ADS1115.set_gain("+b.getFieldValue("GAIN")+")\n";};
+Blockly.Python["ADS1115_read_raw"]=function(b){Blockly.Python.definitions_["import_ADS1115"]="import ADS1115";return["ADS1115.read_raw("+b.getFieldValue("CHANNEL")+")",Blockly.Python.ORDER_FUNCTION_CALL];};
+Blockly.Python["ADS1115_read_mv"]=function(b){Blockly.Python.definitions_["import_ADS1115"]="import ADS1115";return["ADS1115.read_voltage("+b.getFieldValue("CHANNEL")+")",Blockly.Python.ORDER_FUNCTION_CALL];};
+Blockly.Python["ADS1115_read_v"]=function(b){Blockly.Python.definitions_["import_ADS1115"]="import ADS1115";return["ADS1115.read_voltage_v("+b.getFieldValue("CHANNEL")+")",Blockly.Python.ORDER_FUNCTION_CALL];};
+Blockly.Python["ADS1115_read_percent"]=function(b){Blockly.Python.definitions_["import_ADS1115"]="import ADS1115";return["ADS1115.read_percent("+b.getFieldValue("CHANNEL")+")",Blockly.Python.ORDER_FUNCTION_CALL];};
