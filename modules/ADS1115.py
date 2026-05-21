dc248b449379483962ea241d4bb4cfd9ba746579
pcvargas <pcvargas@gmail.com>
2026-05-20 21:52:04 -0500

    Update ADS1115.py

--- modules/ADS1115.py
+++ modules/ADS1115.py
@@ -6,25 +6,19 @@ class ADS1115:
         self.address = address
 
     def read_channel(self, channel):
-        # Configuración: Conversión única, Ganancia +/-4.096V, 128 muestras por segundo
-        # Desplazamos el canal (0-3) para activar los multiplexores correspondientes de A0 a A3
+        # Configuración: Conversión única, Ganancia +/-4.096V, 128 SPS
         config = 0x8283 | ((channel + 4) << 12)
         
         data = bytearray(2)
         data[0] = (config >> 8) & 0xFF
         data[1] = config & 0xFF
         
-        # Escribir en el registro de configuración (0x01)
         self.i2c.writeto_mem(self.address, 0x01, data)
-        
-        # Tiempo prudencial para que finalice la conversión
         time.sleep_ms(10)
         
-        # Leer el registro de conversión de datos (0x00)
         res = self.i2c.readfrom_mem(self.address, 0x00, 2)
         val = (res[0] << 8) | res[1]
         
-        # Manejo del complemento a dos para lecturas negativas
         if val > 32767:
             val -= 65536
         return val
