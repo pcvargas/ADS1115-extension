a20254deacbb59261d602a29c0f8248316e93554
pcvargas <pcvargas@gmail.com>
2026-05-20 22:07:11 -0500

    Update extension.js

--- extension.js
+++ extension.js
@@ -1,67 +1,41 @@
-module.exports = {
-    id: "ads1115_esp32_adc",
-    name: "ADS1115 ADC 16-Bit",
-    description: "Lectura analógica de alta precisión (16 bits) usando el módulo ADS1115 e I2C en la ESP32.",
-    version: "1.0.0",
+({
+    name: "ADS1115 ADC",
+    description: "Lectura analógica de alta precisión de 16 bits usando el chip ADS1115 e I2C.",
     author: "Pablo Cesar Vargas Diaz",
     category: "Sensors",
-    color: "#2C3E50",
+    version: "1.0.0",
     icon: "/static/icon.png",
-    modules: [
-        "modules/ads1115.py"
-    ],
+    color: "#2C3E50",
     blocks: [
         {
-            opcode: "ads1115_init",
-            blockType: "command",
-            text: "Inicializar ADS1115 I2C SDA [SDA] SCL [SCL]",
-            arguments: {
-                SDA: {
-                    type: "dropdown",
-                    options: {
-                        "21": "21",
-                        "22": "22",
-                        "4": "4",
-                        "5": "5"
-                    },
-                    default: "21"
-                },
-                SCL: {
-                    type: "dropdown",
-                    options: {
-                        "22": "22",
-                        "21": "21",
-                        "14": "14",
-                        "15": "15"
-                    },
-                    default: "22"
-                }
-            },
-            generator: (args) => {
-                // Genera el código MicroPython mapeando directamente los argumentos limpios
-                return `import machine\nfrom ads1115 import ADS1115\ntry:\n    i2c_bus\nexcept NameError:\n    i2c_bus = machine.I2C(0, sda=machine.Pin(${args.SDA}), scl=machine.Pin(${args.SCL}))\nadc_ext = ADS1115(i2c_bus)\n`;
-            }
+            xml: `
+                <block type="ads1115_setup">
+                    <value name="sda">
+                        <shadow type="math_number">
+                            <field name="NUM">21</field>
+                        </shadow>
+                    </value>
+                    <value name="scl">
+                        <shadow type="math_number">
+                            <field name="NUM">22</field>
+                        </shadow>
+                    </value>
+                </block>
+            `
         },
         {
-            opcode: "ads1115_read",
-            blockType: "reporter",
-            text: "Leer Canal [CHANNEL] del ADS1115",
-            arguments: {
-                CHANNEL: {
-                    type: "dropdown",
-                    options: {
-                        "A0": "0",
-                        "A1": "1",
-                        "A2": "2",
-                        "A3": "3"
-                    },
-                    default: "0"
-                }
-            },
-            generator: (args) => {
-                // En microBlock v3, los reporteros devuelven simplemente el string de la expresión
-                return `adc_ext.read_channel(${args.CHANNEL})`;
-            }
+            xml: `
+                <block type="ads1115_read">
+                    <value name="channel">
+                        <shadow type="math_number">
+                            <field name="NUM">0</field>
+                        </shadow>
+                    </value>
+                </block>
+            `
         }
+    ],
+    chip: [
+        "ESP32"
     ]
-};
+});
